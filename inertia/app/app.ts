36ded9b30a6040bdb2de0435da9bcd3ccdcc0bef
memsbdm <mehmetbdm@outlook.fr>
2025-05-03 17:25:13 +0200

    chore: setup tailwind, shadcn and unplugin-vue-components

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,11 +1,12 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css';
+import '../css/app.css'
 import { createSSRApp, h } from 'vue'
 import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { Link } from '@inertiajs/vue3'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -17,15 +18,14 @@ createInertiaApp({
   resolve: (name) => {
     return resolvePageComponent(
       `../pages/${name}.vue`,
-      import.meta.glob<DefineComponent>('../pages/**/*.vue'),
+      import.meta.glob<DefineComponent>('../pages/**/*.vue')
     )
   },
 
   setup({ el, App, props, plugin }) {
-    
     createSSRApp({ render: () => h(App, props) })
-    
       .use(plugin)
+      .component('Link', Link)
       .mount(el)
   },
 })
