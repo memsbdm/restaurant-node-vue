4ae2126ea16da61758f69f14b0efed8edf5bfb1c
memsbdm <mehmetbdm@outlook.fr>
2025-05-04 11:24:03 +0200

    feat(auth): add logout

---
+++ app/controllers/auth/logout_controller.ts
@@ -0,0 +1,12 @@
+import Logout from '#actions/auth/logout'
+import { inject } from '@adonisjs/core'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LogoutController {
+  @inject()
+  async handle({ response }: HttpContext, logout: Logout) {
+    await logout.handle()
+
+    return response.redirect().toRoute('auth.login.render')
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -12,6 +12,7 @@ import { middleware } from './kernel.js'
 const HomeController = () => import('#controllers/home_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
 const RegisterController = () => import('#controllers/auth/register_controller')
+const LogoutController = () => import('#controllers/auth/logout_controller')
 
 // Landing page
 router.get('/', [HomeController, 'render']).use(middleware.auth()).as('home.render')
@@ -27,6 +28,11 @@ router
         router.post('/register', [RegisterController, 'handle']).as('register.handle')
       })
       .use(middleware.guest())
+
+    router
+      .delete('/logout', [LogoutController, 'handle'])
+      .use(middleware.auth())
+      .as('logout.handle')
   })
   .prefix('/auth')
   .as('auth')
