c4ad00e1f39028636a956ac5d0fc604b7e2cdf16
memsbdm <mehmetbdm@outlook.fr>
2025-05-11 16:07:35 +0200

    feat(settings): send notification to previous email on email change and store change in email history

--- app/actions/settings/update_email.ts
+++ app/actions/settings/update_email.ts
@@ -1,5 +1,7 @@
 import User from '#models/user'
 import { updateEmailValidator } from '#validators/setting'
+import db from '@adonisjs/lucid/services/db'
+import mail from '@adonisjs/mail/services/main'
 import { Infer } from '@vinejs/vine/types'
 
 type Params = {
@@ -9,11 +11,35 @@ type Params = {
 
 export default class UpdateEmail {
   static async handle({ user, data }: Params) {
+    const emailOld = user.email
+
     await User.verifyCredentials(user.email, data.password)
     user.email = data.email
     user.isVerified = false
     // TODO: delete all confirmation mail tokens
     // TODO: send confirmation mail
-    return user.save()
+    await db.transaction(async (trx) => {
+      user.useTransaction(trx)
+      user.email = data.email
+
+      await user.save()
+      await user.related('emailHistories').create({
+        emailNew: data.email,
+        emailOld,
+      })
+
+      await this.#sendEmailNotification(user, emailOld)
+    })
+
+    return user
+  }
+
+  static async #sendEmailNotification(user: User, emailOld: string) {
+    await mail.sendLater((message) => {
+      message
+        .to(emailOld)
+        .subject('Your email has been successfully changed')
+        .htmlView('emails/email_change', { user })
+    })
   }
 }
